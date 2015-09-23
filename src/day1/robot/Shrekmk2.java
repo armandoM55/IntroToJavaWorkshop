26e8b56692fb60fd2b96eada7f3ae797c0684b7b
league-workshopper <workshop@Administrators-Mac-mini.local>
2015-09-23 15:00:33 -0700

    shrek
    trail n error

---
+++ src/day1/robot/Shrekmk2.java
@@ -0,0 +1,16 @@
+package day1.robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Shrekmk2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (int x = 0; x < 4; x++) { 
+	Robot Edwin=new Robot(); 
+	Edwin.move(30);
+	
+}
+	}
+
+}
---
+++ src/day1/robot/Square.java
@@ -0,0 +1,23 @@
+package day1.robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Square {
+
+
+public static void main(String[] args) {
+	Robot ironman=new Robot(); 
+	ironman.setSpeed(90001);
+	ironman.setPenWidth(5000);
+	for (int i = 0; i < 100; i++) {  
+		ironman.penDown();
+		ironman.setRandomPenColor(); 
+		for (int j = 0; j < 4; j++) { 
+			ironman.move(100);
+			ironman.turn(90); 
+		} 
+		
+		
+	}
+}
+}
